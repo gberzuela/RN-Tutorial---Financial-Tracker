71efa6a18e72913cfffe4507ca5316c6f7bfe96a
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2020-12-29 19:32:05 -0800

    adds notes on navigation

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import HomePage from './HomePage';
 import LoginPage from './LoginPage';
 import { NavigationContainer } from '@react-navigation/native';
+// ^^ loosely equals to Router from react-router-dom
 import { createStackNavigator } from '@react-navigation/stack';
 
 // Think of this stack as the history from React Routers
 // push/pop screens off to navigate
 const Stack = createStackNavigator();
+// Stack.Screen loosely equals to Route from react-router-dome
 
 const App = () => {
 	return (
